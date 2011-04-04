36488a49fbe4bc751f85e8b6e74df50e11487a7e
binaryblade <binaryblade@375071b8-0b05-4826-8f69-d66525b871ba>
2011-04-04 00:20:52 +0000

    simple region size checking
    git-svn-id: https://svn.thecube.ca/svn/minecraftpp@23 375071b8-0b05-4826-8f69-d66525b871ba

--- trunk/src/mapper.cpp
+++ trunk/src/mapper.cpp
@@ -1,5 +1,16 @@
 #include "world.h"
+#include <iostream>
 
 int main(int argc,char *argv[]) {
+	if(argc != 3) {
+		std::cout << "Correct use is: \n \t mapper world_dir blockID" << std::endl;
+		return -1;
+	}
+	std::string worlddir(argv[1]);
+	World map(worlddir);
+	Coord tL = map.getTopLeft();
+	Coord bR = map.getBottomRight();
+	std::cout << "Width: " << tL.second - bR.second << std::endl;
+	std::cout << "Height: " << bR.first - tL.first << std::endl;
 	return 0;
 }
