61918e6af9c400adaf14b62143ce1730c52768c6
binaryblade <binaryblade@375071b8-0b05-4826-8f69-d66525b871ba>
2011-04-07 04:38:00 +0000

    begin retooling chunk_reader to make blobs
    git-svn-id: https://svn.thecube.ca/svn/minecraftpp@32 375071b8-0b05-4826-8f69-d66525b871ba

--- trunk/src/chunk_reader.cpp
+++ trunk/src/chunk_reader.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdio>
+#include <cstring>
 #include <map>
 #include <vector>
 
@@ -36,45 +37,91 @@ const uint8_t trackedblocks[] = {
 	BLOCK_PUMPKIN
 };
 
-typedef std::vector<location> LocVector;
-typedef std::map<uint8_t,LocVector> CoordMap;
-typedef std::pair<uint8_t,LocVector> MapItem;
+const uint32_t ABOVE = 1<<0;
+const uint32_t BELOW = 1<<1;
+const uint32_t WEST = 1<<2;
+const uint32_t EAST = 1<<3;
+const uint32_t NORTH = 1<<4;
+const uint32_t SOUTH = 1<<5;
+const uint32_t NORTH_WEST = 1<<6;
+const uint32_t NORTH_EAST = 1<<7;
+const uint32_t SOUTH_WEST = 1<<8;
+const uint32_t SOUTH_EAST = 1<<9;
+const uint32_t ABOVE_WEST = 1<<10;
+const uint32_t ABOVE_EAST = 1<<11;
+const uint32_t ABOVE_NORTH = 1<<12;
+const uint32_t ABOVE_SOUTH = 1<<13;
+const uint32_t ABOVE_NORTH_WEST = 1<<14;
+const uint32_t ABOVE_NORTH_EAST = 1<<15;
+const uint32_t ABOVE_SOUTH_WEST = 1<<16;
+const uint32_t ABOVE_SOUTH_EAST = 1<<17;
+const uint32_t BELOW_WEST = 1<<18;
+const uint32_t BELOW_EAST = 1<<19;
+const uint32_t BELOW_NORTH = 1<<20;
+const uint32_t BELOW_SOUTH = 1<<21;
+const uint32_t BELOW_NORTH_WEST = 1<<22;
+const uint32_t BELOW_NORTH_EAST = 1<<23;
+const uint32_t BELOW_SOUTH_WEST = 1<<24;
+const uint32_t BELOW_SOUTH_EAST = 1<<25;
 
+
+
+
+
+class block {
+	public:
+		int32_t x;
+		int32_t z;
+		uint8_t y;
+		uint32_t facesChecked;
+		uint8_t openFaces;
+		block(uint8_t blockid , int32_t x,int32_t z,uint8_t y) :x(x),z(z),y(y),facesChecked(0),openFaces(26) {};
+};
+
+class blob {
+	public:
+		std::vector<block> exposed_blocks;
+		int32_t x;
+		int32_t z;
+		uint8_t y;
+		uint16_t count;
+};
+
+bool tracked[256];
+
+typedef std::vector<blob> BlobVector;
 
 int main(void) {
 	World B("./world");
-	CoordMap points;
 	Coord tR = B.getTopRight();
 	Coord bL = B.getBottomLeft();
-	std::map<uint8_t,uint64_t> counts;
+	BlobVector blobs; 
 	
+	memset(tracked,0,sizeof(tracked));
 	for(uint8_t i=0;i<sizeof(trackedblocks);i++) {
-		points.insert(MapItem(trackedblocks[i],LocVector()));
-		counts.insert(std::pair<uint8_t,uint64_t>(trackedblocks[i],0));
+		tracked[trackedblocks[i]]=true;
 	}
 
-	for(int64_t regionX=tR.first;regionX<bL.first;regionX++)
-		for(int64_t regionZ=tR.second;regionZ<bL.second;regionZ++) {
+	for(int32_t regionX=tR.first;regionX<bL.first;regionX++)
+		for(int32_t regionZ=tR.second;regionZ<bL.second;regionZ++) {
 			if(B.regionInDir(regionX,regionZ)) {
 				const RegionInterface &A = B.getRegion(regionX,regionZ);
 				for(int chunkZ=0;chunkZ<REGIONZ;chunkZ++)
 					for(int chunkX=0;chunkX<REGIONX;chunkX++) {
 						if(A.chunkInFile(chunkX,chunkZ)) {
 						const ChunkInterface &temp = A.getChunk(chunkX,chunkZ);
+						bool visited[16][128][16];
+						memset(visited,0,sizeof(visited));
 						for(int z=0;z<CHUNKZ;z++)
 							for(int x=0;x<CHUNKX;x++) {
-								int64_t xloc = regionX*32*16 + chunkX*16 + x;
-								int64_t zloc = regionZ*32*16 + chunkZ*16 + z;
+								int32_t xloc = regionX*32*16 + chunkX*16 + x;
+								int32_t zloc = regionZ*32*16 + chunkZ*16 + z;
 								for(uint8_t y=0;y<128;y++) {
-									uint8_t block = temp.getBlock(x,y,z);
-									location loc;
-									loc.x = xloc;
-									loc.y = y;
-									loc.z = zloc;
-									CoordMap::iterator iter = points.find(block);
-									if( iter != points.end()) {
-											iter->second.push_back(loc);	
-											counts.find(block)->second++;
+									if(!visited[x][y][z]) {
+										visited[x][y][z] = true;
+										uint8_t block = temp.getBlock(x,y,z);
+										if(tracked[block]) {											
+										}
 									}
 								}	
 							}
@@ -84,7 +131,7 @@ int main(void) {
 			}
 		}
 	
-	
+	/*
 	pqxx::connection Conn("dbname=minecraft");	
 	std::vector<std::string> names;
 	names.push_back("id");
@@ -108,6 +155,6 @@ int main(void) {
 		table.complete();
 		trans.commit();
 	}
-		
+		*/
 	return 0;
 }
