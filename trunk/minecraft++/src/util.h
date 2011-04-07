849ddc61f1bfa839dd129e4d0b977485a2b9f971
binaryblade <binaryblade@375071b8-0b05-4826-8f69-d66525b871ba>
2011-04-07 22:25:15 +0000

    util comments woot
    git-svn-id: https://svn.thecube.ca/svn/minecraftpp@38 375071b8-0b05-4826-8f69-d66525b871ba

--- trunk/minecraft++/src/util.h
+++ trunk/minecraft++/src/util.h
@@ -8,6 +8,9 @@
   * converts the first 2 bytes of the array 
 	* into a short, and increments the pointer 
 	* by 2
+	*
+	* @param data pointer to a bigendian encoded int16_t
+	*
   **/ 
 int16_t arrayToShort(uint8_t *&data); 
 
@@ -15,6 +18,9 @@ int16_t arrayToShort(uint8_t *&data);
   * converts the first 4 bytes of the array 
 	* into an int, and increments the pointer 
 	* by 4
+	*
+	*	@param data pointer to a bigendian encoded int32_t
+	*
   **/ 
 int32_t arrayToInt(uint8_t *&data);
 
@@ -22,6 +28,9 @@ int32_t arrayToInt(uint8_t *&data);
   * converts the first 8 bytes of the array 
 	* into a Long, and increments the pointer 
 	* by 8
+	*
+	*	@param data pointer to a bigendian encoded int64_t
+	*
   **/ 
 int64_t arrayToLong(uint8_t *&data); 
 
@@ -32,17 +41,94 @@ int64_t arrayToLong(uint8_t *&data);
 	* it then reads int he string, out of the reast of
 	* the data, the string is not null terminated
 	* it then increments the pointer 2 + stringlength
+	*
+	*	@param data pointer to first a bigendian int16_t 
+	*             and then a non NULL terminated string
+	*
   **/ 
 std::string arrayToString(uint8_t *&data);
 
+/**
+  * takes a pointer into an array of bytes
+  * converts the first 4 bytes of the array
+  * into a float and increments the pointer
+	* by 4
+	*
+	* @param data pointer to an IEEE bigendian float 
+  *
+	**/
 float arrayToFloat(uint8_t *&data);
+
+/**
+  * takes a pointer into an array of bytes
+  * converts the first 8 bytes of teh array
+  * into a double and increments the pointer
+  * by 8
+	*
+  * @param data pointer to an IEEE bigendian double
+  *
+  **/
 double arrayToDouble(uint8_t *&data);
 
+/** takes a pointer to an array and a string reference
+  * inserts the length of the string via shortToArray
+  * then writes the string to the array non null terminated
+  *
+  * @param data pointer to a large enough data array to store string and size
+	*
+	* @param text string to insert into the array
+  *
+  **/
+unsigned int stringToArray(uint8_t *&data,const std::string &text);
 
-unsigned int stringToArray(uint8_t *&,const std::string &);
-void longToArray(uint8_t *&, int64_t);
-void intToArray(uint8_t *&,int32_t);
-void shortToArray(uint8_t *&,int16_t);
-void floatToArray(uint8_t *&,float);
+/** takes a pointer to an array and inserts an int64_t into it
+  * the int is stored in big endian, also increments pointer by 8
+  *
+  * @param data pointer to the array
+	*
+	* @param number value to place in array
+  *
+  **/
+void longToArray(uint8_t *&data, int64_t number);
+
+
+/** takes a pointer to an array and inserts an int32_t into it
+  * the int is stored in big endian, also increments pointer by 4
+  *
+  * @param data pointer to an array
+	*
+	* @param number number to insert
+  *
+  **/
+void intToArray(uint8_t *&data,int32_t number);
+
+/** takes a pointer to an array and inserts an int16_t into it
+  * the int is stored in big endian, also increments the pointer by 2
+  *
+  * @param data pointer to array
+	*
+	* @param number number to insert
+  *
+  **/
+void shortToArray(uint8_t *&data,int16_t number);
+
+/** takes a pointer to an array and inserts a big endian
+  * IEEE float into, also increments the pointer by 4
+  *
+  * @param data pointer to array
+  *
+  * @param number the float to insert
+  *
+  **/
+void floatToArray(uint8_t *&data,float number);
+
+/** takes a pointer to an array and inserts a big endian
+  * IEEE double into it, also increments the pointer by 8
+  *
+  * @param data pointer to array
+  *
+	* @param number number to insert
+  *
+  **/
 void doubleToArray(uint8_t *&,double);
 #endif
